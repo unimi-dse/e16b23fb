0e7c21f34023f6f800374408ebf5fcf12b321d6b
nicholascarp <nicholas.carp@outlook.it>
2020-02-12 05:31:37 +0100

    Update plot_stockp.R, no "" in parameters documentation

--- R/plot_stockp.R
+++ R/plot_stockp.R
@@ -2,8 +2,8 @@
 #'
 #' Plots a graph of the closing price vector.
 #'
-#' @param "data" dataframe. The stock dataset that you want to used. If \code{NULL} (the default) selects TSLA  dataset.
-#' @param "title" character. title that you want to assign to the graph.
+#' @param data dataframe. The stock dataset that you want to used. If \code{NULL} (the default) selects TSLA  dataset.
+#' @param title character. title that you want to assign to the graph.
 #'
 #' @return A graph of the stock price
 #'
@@ -11,6 +11,8 @@
 #' plot_stockp()
 #' plot_stockp(aapl)
 #' plot_stockp(tsla,TSLA)
+#'
+#'
 #' @export
 #'
 #'
@@ -20,7 +22,7 @@ plot_stockp <- function(data=NULL,title=""){
   if(is.null(data))
   data <-get_data()
   data %>%
-    ggplot2::ggplot(aes(x = date, y = close)) +
+    ggplot(aes(x = date, y = close)) +
     geom_line() +
     labs(title = title, y = "Closing Price", x = "Years") +
     theme_tq()
