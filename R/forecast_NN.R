6db2d4953f3fc80833ab7fe6eccf11703a47084a
nicholascarp <nicholas.carp@outlook.it>
2020-02-12 05:32:40 +0100

    Update forecast_NN.R, return changed

--- R/forecast_NN.R
+++ R/forecast_NN.R
@@ -1,9 +1,9 @@
 #' forecast_NN
 #'
 #' Performs a neural network forecasting
-#' @param "data" The stock dataset that you want to use. If \code{NULL} (the default) selects TSLA  dataset.
+#' @param data dataframe. The stock dataset that you want to use. If \code{NULL} (the default) selects TSLA  dataset.
 #'
-#' @return graph showing forecasted values
+#' @return Dataframe of the forecasted values and their graph
 #'
 #' @examples
 #' forecast_NN()
@@ -16,8 +16,9 @@ forecast_NN <- function(data=NULL) {
     data <-get_data()
   alpha <- 1.5^(-10)
   hn <- length(data$close)/(alpha*(length(data$close)+30))
-  lambda <- BoxCox.lambda(data$close)
-  dnn_pred <- nnetar(data$close, size= hn, lambda = lambda)
-  dnn_forecast <- forecast(dnn_pred, h= 30, PI = TRUE)
+  lambda <- forecast::BoxCox.lambda(data$close)
+  dnn_pred <- forecast::nnetar(data$close, size= hn, lambda = lambda)
+  dnn_forecast <- forecast::forecast(dnn_pred, h= 30, PI = TRUE)
   plot(dnn_forecast)
+  return(dnn_forecast)
 }
