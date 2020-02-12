ae06bf1be48a178fd1d1726bf6e368ecd1584c56
nicholascarp <nicholas.carp@outlook.it>
2020-02-12 05:30:33 +0100

    Update get_data.R ,Parameters without ""

--- R/get_data.R
+++ R/get_data.R
@@ -5,9 +5,9 @@
 #' You have to store the result in a variable in order to be able to perform further analisys
 #' with the stock that you are intrested in
 #'
-#' @param "id" character. Ticker of the desired stock. If \code{NULL} selects TSLA  dataset.
-#' @param "from" date. The date you want to start. If \code{NULL} selects "2012-09-01"
-#' @param "to" date. The date you want to stop. If \code{NULL} selects today's date
+#' @param id character. Ticker of the desired stock. If \code{NULL} selects TSLA  dataset.
+#' @param from date. The date you want to start. If \code{NULL} selects "2012-09-01"
+#' @param to date. The date you want to stop. If \code{NULL} selects today's date
 #' @return A dataset of the selected stock
 #'
 #'
